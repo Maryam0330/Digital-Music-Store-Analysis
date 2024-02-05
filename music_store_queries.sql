e497cd177358738907d322649ae10fd20a0418bb
Maryam Panjri <maryampanjri63@gmail.com>
2024-02-05 16:49:30 +0530

    Update music_store_queries.sql

--- music_store_queries.sql
+++ music_store_queries.sql
@@ -132,3 +132,90 @@ WHERE milliseconds > (
 	SELECT AVG(milliseconds) AS avg_song_length
   FROM track)
 ORDER BY milliseconds DESC
+
+
+/* Question Set 3 - Advance */
+
+/* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
+
+WITH best_selling_artist AS(
+	SELECT ar.artist_id, ar.name, SUM(il.unit_price * il.quantity) AS total_spent
+	FROM artist ar
+	JOIN album al ON ar.artist_id = al.artist_id
+    JOIN track t ON al.album_id = t.album_id
+    JOIN invoice_line il ON t.track_id = il.track_id 
+    GROUP BY 1, 2
+    ORDER BY 3 DESC
+    LIMIT 1
+)
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    bsa.name AS artist_name,
+    SUM(il.unit_price * il.quantity) AS total_spent
+FROM customer c
+JOIN invoice i ON c.customer_id = i.customer_id
+JOIN invoice_line il ON i.invoice_id = il.invoice_id
+JOIN track t ON il.track_id = t.track_id
+JOIN album al ON t.album_id = al.album_id
+JOIN best_selling_artist bsa ON al.artist_id = bsa.artist_id
+GROUP BY 1, 2, 3, 4
+ORDER BY 5 DESC
+
+
+/* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
+with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
+the maximum number of purchases is shared return all Genres. */
+
+/* Using CTE method */
+
+WITH popular_music_genre AS(
+	SELECT 
+		COUNT(il.quantity) AS purchases, 
+		c.country,
+        	g.genre_id,
+        	g.name,
+		ROW_NUMBER() OVER (PARTITION BY c.country ORDER BY COUNT(il.quantity) DESC) AS row_num
+    FROM customer c
+    JOIN invoice i ON c.customer_id = i.customer_id
+    JOIN invoice_line il ON i.invoice_id = il.invoice_id
+    JOIN track t ON il.track_id = t.track_id
+    JOIN genre g ON t.genre_id = g.genre_id
+    GROUP BY 2, 3, 4
+    ORDER BY 
+		2 ASC, 
+		1 DESC
+    )
+    
+SELECT *
+FROM popular_music_genre
+WHERE row_num <= 1
+
+	
+/* Q3: Write a query that determines the customer that has spent the most on music for each country. 
+Write a query that returns the country along with the top customer and how much they spent. 
+For countries where the top amount spent is shared, provide all customers who spent this amount. */
+
+/* Using CTE method */ 
+
+WITH highest_spending_customer AS(
+	SELECT
+		c.customer_id,
+        c.first_name,
+        c.last_name,
+        i.billing_country,
+        SUM(i.total) AS total_spending,
+        ROW_NUMBER() OVER (PARTITION BY i.billing_country ORDER BY SUM(i.total) DESC) AS row_num
+    FROM customer c
+    JOIN invoice i ON c.customer_id = i.customer_id
+    GROUP BY 1, 2, 3, 4
+    ORDER BY 
+		4 ASC, 
+        5 DESC
+        )
+    
+SELECT *
+FROM highest_spending_customer
+WHERE row_num <= 1
